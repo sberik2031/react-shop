ef20d5c31a89ebac02b6f10f093b640c3e75b2d8
unknown <berik2031@gmail.com>
2022-10-16 23:24:56 +0600

    fix reducer

---
+++ src/reducer.js
@@ -0,0 +1,13 @@
+export function reducer(state, { type, payload }) {
+    switch (type) {
+        case 'REMOVE_FROM_BASKET':
+            return {
+                ...state,
+                order: state.order.filter((el) => el.id !== payload.id),
+            };
+        case 'CLOSE_ALERT':
+            return { ...state, alertName: '' };
+        default:
+    }
+    return state;
+}
